9c90d8ff3901aa8e7c8b5d1327e3d9090570244a
Ganesh gowda <gowdaganesh0511@gmail.com>
2024-08-10 19:48:37 +0530

    frontend form setup for backend comunication and backend mongoose models for the mongodb

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,6 +6,8 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} f
 import Home from './Components/Home/Home.jsx'
 import DangerButton from './Components/Danger/DangerButton.jsx'
 import Dangerform from './Components/Danger/Dangerform.jsx'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const router=createBrowserRouter(
@@ -24,8 +26,20 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}>
     <App />
+   
     
   </RouterProvider>
+  <ToastContainer 
+     position="top-right" // Position of the toast notifications
+     autoClose={5000} // Auto-close after 5 seconds
+     hideProgressBar={false} // Show progress bar
+     newestOnTop={false} // Newest toast on top
+     closeOnClick // Allow closing by clicking
+     rtl={false} // Right-to-left language
+     pauseOnFocusLoss // Pause when focus is lost
+     draggable // Allow dragging
+     pauseOnHover
+    />
   
   </React.StrictMode>,
 )
